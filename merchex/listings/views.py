78ec27f0406c8ea72a3285e88cce5bab2b812841
Armen <asnet11@gmail.com>
2023-10-27 23:43:40 +0200

    fin partie 1

--- merchex/listings/views.py
+++ merchex/listings/views.py
@@ -5,30 +5,21 @@ from listings.models import Listing
 
 def hello(request):
     bands = Band.objects.all()
-    return HttpResponse(f"""
-        <h1>Hello Django!</h1>
-        <p>Mes groupes preférés sont :</p>
-        <ul>
-            <li>{bands[0].name}</li>
-            <li>{bands[1].name}</li>
-            <li>{bands[2].name}</li>
-        </ul>
-    """)
+    return render(request,
+                'listings/hello.html',
+                {'bands': bands},
+            )
 
 def about(request):
-    return HttpResponse('<h1>A propos</h1> <p>Nous adorons Merch !</p>')
+    return render(request,
+                  'listings/about.html')
 
 def listings(request):
-    lisitings = Listing.objects.all()
-    return HttpResponse(f"""
-        <h1>Listings</h1>
-        <p>C\'est la page Listings.</p>
-        <ul>
-            <li>{lisitings[0].title}</li>
-            <li>{lisitings[1].title}</li>
-            <li>{lisitings[2].title}</li>
-        </ul>
-    """)
+    listings = Listing.objects.all()
+    return render(request,
+                  'listings/listings.html',
+                  {'listings': listings},
+                )
 
 def contact(request):
-    return HttpResponse('<h1>Contactez-nous !</h1> <p>Vous êtes sur la page de contac !</p>')
+    return render(request, 'listings/contact.html')
